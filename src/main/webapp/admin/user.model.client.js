3ba4e51b038735787fd9855d5347889cf167b091
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-09-23 22:55:13 -0400

    fixing client User model

--- src/main/webapp/admin/user.model.client.js
+++ src/main/webapp/admin/user.model.client.js
@@ -1,18 +1,20 @@
-function User(username, password, firstName, lastName) {
-    this.username = username;
-    this.password = password;
-    // ...same for rest of properties…
-
-    this.setUsername = setUsername;
-    this.getUsername = getUsername;
-    // ...same for rest of properties…
-
-    function setUsername(username) {
+export class User {
+    constructor(username, password, firstName, lastName) {
         this.username = username;
-    }
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
 
-    function getUsername() {
-        return this.username;
+        this.setUsername = setUsername;
+        this.getUsername = getUsername;
+        // ...same for rest of properties…
+        function setUsername(username) {
+            this.username = username;
+        }
+
+        function getUsername() {
+            return this.username;
+        }
+        // ...same for rest of properties…
     }
-    // ...same for rest of properties…
 }
