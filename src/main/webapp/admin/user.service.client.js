28376a21abad44dfd54060773ad0cded4cbcd09a
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-09-24 21:20:47 -0400

    adding functionality to find user by id

--- src/main/webapp/admin/user.service.client.js
+++ src/main/webapp/admin/user.service.client.js
@@ -1,7 +1,7 @@
 function AdminUserServiceClient() {
     this.createUser = createUser;
     this.findAllUsers = findAllUsers;
-    // this.findUserById = findUserById;
+    this.findUserById = findUserById;
     // this.deleteUser = deleteUser;
     // this.updateUser = updateUser;
     this.url = 'https://wbdv-generic-server.herokuapp.com/api/001289771';
@@ -33,7 +33,16 @@ function AdminUserServiceClient() {
     }
 
     //retrieves a single user object whose id is equal to the id parameter
-    // function findUserById(userId, callback) {…}
+    function findUserById(userId, callback) {
+        url = this.url + '/users/' + userId;
+        console.log('url')
+        fetch(url, {
+            method: 'get',
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(response => response.json().then(parsedJson => callback(parsedJson)))
+    }
 
     //accepts a user id and user object with new property values for the user with user id.
     // Finds the user whose id matches he id parameter and updates it with the values in the user parameter
