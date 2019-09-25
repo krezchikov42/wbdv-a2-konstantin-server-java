cb8c681842e7631379925e7bcb237bdcdc64bf7c
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-09-24 21:42:28 -0400

    adding delete and update user to server code

--- src/main/webapp/admin/user.service.client.js
+++ src/main/webapp/admin/user.service.client.js
@@ -2,16 +2,16 @@ function AdminUserServiceClient() {
     this.createUser = createUser;
     this.findAllUsers = findAllUsers;
     this.findUserById = findUserById;
-    // this.deleteUser = deleteUser;
-    // this.updateUser = updateUser;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
     this.url = 'https://wbdv-generic-server.herokuapp.com/api/001289771';
     var self = this;
 
     //accepts a user object and adds it to a collection of users
     function createUser(user, callback) {
-        url = this.url + '/users';
+        let url = this.url + '/users';
         let promise = fetch(url, {
-            method: 'post',
+            method: 'POST',
             body: JSON.stringify(user),
             headers: {
                 'content-type': 'application/json'
@@ -21,9 +21,9 @@ function AdminUserServiceClient() {
 
     //retrieves all users as an array of JSON objects
     function findAllUsers(callback) {
-        url = this.url + '/users';
+        let url = this.url + '/users';
         let promise = fetch(url, {
-            method: 'get',
+            method: 'GET',
             headers: {
                 'content-type': 'application/json'
             }
@@ -32,10 +32,9 @@ function AdminUserServiceClient() {
 
     //retrieves a single user object whose id is equal to the id parameter
     function findUserById(userId, callback) {
-        url = this.url + '/users/' + userId;
-        console.log('url')
+        let url = this.url + '/users/' + userId;
         fetch(url, {
-            method: 'get',
+            method: 'GET',
             headers: {
                 'content-type': 'application/json'
             }
@@ -44,8 +43,26 @@ function AdminUserServiceClient() {
 
     //accepts a user id and user object with new property values for the user with user id.
     // Finds the user whose id matches he id parameter and updates it with the values in the user parameter
-    // function updateUser(userId, user, callback) {…}
+    function updateUser(userId, user, callback) {
+        let url = this.url + '/users/' + userId;
+        fetch(url, {
+            method: 'PUT',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(response => response.json().then(parsedJson => callback(parsedJson)))
+    }
 
     //removes the user whose id matches the id parameter
-    // function deleteUser(userId, callback) {…}
+    function deleteUser(userId, callback) {
+        let url = this.url + '/users/' + userId;
+        fetch(url, {
+            method: 'DELETE',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(response => response.json().then(parsedJson => callback(parsedJson)))
+    }
 }
