86f76c18ec1d96f5cdec4247c48708a9aa6ca508
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-11-04 13:24:39 -0500

    adding enums to define widgets

---
+++ src/main/java/com/example/wbdvf18jannunziserverjava/models/WidgetDataType.java
@@ -0,0 +1,8 @@
+package com.example.wbdvf18jannunziserverjava.models;
+
+public enum WidgetDataType {
+    INTEGER,
+    STRING,
+    DATA,
+    BOOLEAN
+}
---
+++ src/main/java/com/example/wbdvf18jannunziserverjava/models/WidgetType.java
@@ -0,0 +1,11 @@
+package com.example.wbdvf18jannunziserverjava.models;
+
+public enum WidgetType {
+    HEADING,
+    LIST,
+    PARAGRAPH,
+    IMAGE,
+    YOUTUBE,
+    HTML,
+    LINK
+}
