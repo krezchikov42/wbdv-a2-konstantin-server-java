b0d54f6a6557ed4b907c065618bf6dd843e781ab
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-11-03 23:21:15 -0500

    adding topics model

---
+++ src/main/java/com/example/wbdvf18jannunziserverjava/models/Topic.java
@@ -0,0 +1,39 @@
+package com.example.wbdvf18jannunziserverjava.models;
+
+import java.util.List;
+
+public class Topic {
+    Integer id;
+    List<Widget> widgetMany;
+    String title;
+
+    public Topic(Integer id) {
+        this.id = id;
+    }
+
+    public Topic(Integer id, List<Widget> widgetMany, String title) {
+        this.id = id;
+        this.widgetMany = widgetMany;
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public List<Widget> getWidgetMany() {
+        return widgetMany;
+    }
+
+    public void setWidgetMany(List<Widget> widgetMany) {
+        this.widgetMany = widgetMany;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
