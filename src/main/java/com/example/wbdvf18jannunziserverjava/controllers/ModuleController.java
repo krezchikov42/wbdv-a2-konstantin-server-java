e883ff2ceb2e35dab9bf5312cfab8c69bb999935
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-11-15 17:04:21 -0500

    adding Module controller methods

--- src/main/java/com/example/wbdvf18jannunziserverjava/controllers/ModuleController.java
+++ src/main/java/com/example/wbdvf18jannunziserverjava/controllers/ModuleController.java
@@ -5,10 +5,7 @@ import com.example.wbdvf18jannunziserverjava.models.Module;
 import com.example.wbdvf18jannunziserverjava.repositories.CourseRepository;
 import com.example.wbdvf18jannunziserverjava.repositories.ModuleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +15,50 @@ public class ModuleController {
     @Autowired
     ModuleRepository repository;
 
+    @Autowired
+    CourseRepository courseRepository;
+
     @GetMapping("/api/courses/{courseId}/modules")
     public List<Module> findAllCourses(
             @PathVariable("courseId") Integer courseId) {
         return repository.findModulesForCourse(courseId);
+    }
+
+    @PostMapping("api/courses/{courseId}/modules")
+    public List<Module> createModule(
+            @PathVariable("courseId") Integer courseId,
+            @RequestBody Module module
+    ){
+        Course course = courseRepository.findCourseById(courseId);
+        module.setCourse(course);
+        repository.save(module);
+        return course.getModuleMany();
+    }
+
+    @GetMapping("api/modules/{moduleId}")
+    public Module findModuleById(
+            @PathVariable("moduleId") Integer moduleId
+    ){
+        Module module = repository.findModuleForId(moduleId);
+        return  module;
+    }
+
+    @PutMapping("api/modules/{moduleId}")
+    public List<Module> updateModule(
+            @PathVariable("moduleId") Integer moduleId,
+            @RequestBody Module module
+    ){
+        repository.save(module);
+        Integer courseId =  module.getCourse().getId();
+        return repository.findModulesForCourse(courseId);
+    }
+
+    @DeleteMapping("api/modules/{moduleId}")
+    public List<Module> deleteModule(
+            @PathVariable("moduleId") Integer moduleId
+            ){
+        Integer courseId = repository.findModuleForId(moduleId).getCourse().getId();
+        repository.deleteById(moduleId);
+        return  repository.findModulesForCourse(courseId);
     }
 }
